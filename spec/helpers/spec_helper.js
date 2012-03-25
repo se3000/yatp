1c9fff37b57b508974390a8cf4fcd30cdcd5b5bb
Steve Ellis <sellis6688@gmail.com>
2012-03-25 02:01:08 -0400

    blur and hide the date picker after each test

--- spec/helpers/spec_helper.js
+++ spec/helpers/spec_helper.js
@@ -10,6 +10,10 @@ window.content = function content(html) {
   return $('div#jasmine_content').html(html).contents();
 }
 
+beforeEach(function () {
+  $(".datepicker, .datetimepicker").val('').blur().datepicker('hide');
+});
+
 afterEach(function () {
   $(".datepicker, .datetimepicker").val('').blur().datepicker('hide');
   $("#jasmine_content").empty();
